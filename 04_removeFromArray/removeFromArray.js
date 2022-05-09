5d47d936923fcb28a4e6bb39f37171a44f9f3c01
Jack Munhall <jman777jman@gmail.com>
2022-05-08 23:42:27 -0700

    Completed 1-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(ary, ...values) {
+    for (let i = 0; i < values.length; i++) {
+        value = values[i];
+        if (ary.includes(value)) {
+            ary.splice(ary.indexOf(value),1);
+        }
+    }
+    return ary;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0) return "ERROR";
+    if (typeof(start) != "number" || typeof(end) != "number") return "ERROR";
+    let sum = 0, first, last;
+    if (start < end) first = start, last = end;
+    else first = end, last = start;
+    for (first; first <= last; first++) {
+        sum += first;
+    }
+    return sum;
 };
 
 // Do not edit below this line
